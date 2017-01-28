45c7ee2420d679c36b982e6ec93de461435d622b
Juan Pedro Gutierrez <juanpedrogutierrez@Juans-MacBook-Pro-2.local>
2017-01-28 19:38:54 +0100

    Traps

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour {
+
+	// Use this for initialization
+	public Player player;
+	public bool active;
+	void Start () {
+		player = FindObjectOfType<Player> ();
+		active = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		
+		if (col.gameObject.name == "Character" && active) {
+			Debug.Log ("Triggerd Trap");
+			SpriteRenderer sprite = gameObject.GetComponent<SpriteRenderer> ();
+			sprite.color = new Color (0, 0, 0);
+
+
+			player.HealthSystem.Damage(10);
+			active = false;
+
+		}
+	}
+}

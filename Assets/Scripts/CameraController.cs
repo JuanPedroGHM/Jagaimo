b04402d198d737ea520cbfc6431ea27c12be27d2
Custodes117 <masterdiver117@yahoo.de>
2017-01-28 18:13:34 +0100

    add camera controller to camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,11 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CompleteCameraController : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
-
 	public GameObject player;       //Public variable to store a reference to the player game object
-
 
 	private Vector3 offset;         //Private variable to store the offset distance between the player and camera
 
